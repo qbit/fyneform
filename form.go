362c3eb718d80762d66d81285da46d72a7ac2b82
Aaron Bieber <aaron@bolddaemon.com>
2022-02-25 19:55:45 -0700

    add some docs

--- form.go
+++ form.go
@@ -1,3 +1,8 @@
+/*
+Package fyneform is a library to generate a Fyne Form from a struct. The
+generated form will be bound to the struct members such that updates to the
+form are reflected in the struct.
+*/
 package fyneform
 
 import (
@@ -9,10 +14,16 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+// FormOpts specifies various options for building the form.
 type FormOpts interface {
+	// Titleize will call strings.Title() on a given name. This is useful
+	// if one wants to make JSON names more user friendly.
 	Titelize() bool
 }
 
+// MakeForm takes a struct and builds a set of Fyne form items from it. Types
+// will be matched to the nearest appropriate Item. For example a bool will
+// result in a widget.NewCheckWithData, string in a widgetNewEntryWIthData.
 func MakeForm(st interface{}, opts FormOpts) (items []*widget.FormItem, err error) {
 	stVal := reflect.ValueOf(st)
 	strType := reflect.Indirect(stVal).Type()
@@ -55,6 +66,8 @@ func MakeForm(st interface{}, opts FormOpts) (items []*widget.FormItem, err erro
 
 				items = append(items, widget.NewFormItem(name, w))
 			}
+		default:
+			return nil, fmt.Errorf("unsupported struct member: %q", field.Kind())
 		}
 	}
 	return items, nil
